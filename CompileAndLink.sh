b32f14c52bab4dd4172e22a10221d0832faadbee
Your Name <you@example.com>
2017-03-08 12:55:39 +0900

    fix

--- CompileAndLink.sh
+++ CompileAndLink.sh
@@ -1,7 +1,8 @@
-rm *.so rf
+rm *.so deepforest
 clang++ -std=c++14 RandGen.cpp -shared  -o librandgen.so -fPIC
 clang++ -std=c++14 TreeNode.cpp -shared  -o libtreenode.so -fPIC
 clang++ -std=c++14 DecisionTree.cpp -shared  -o libdecisiontree.so -fPIC
+clang++ -std=c++14 RandomTree.cpp -shared  -o librandomtree.so -fPIC
 clang++ -std=c++14 RandomForest.cpp -shared  -o librandomforest.so -fPIC
-clang++ reset.cpp -std=c++14 -I./ -I/usr/include/boost -L./ -lrandgen -ltreenode -ldecisiontree -lrandomforest -o rf -v 
-cat ./iris.txt | ./rf
+clang++ DeepForest.cpp -std=c++14 -I./ -I/usr/include/boost -L./ -lrandgen -ltreenode -lrandomtree -ldecisiontree -lrandomforest -o deepforest -v 
+cat ./iris.txt | ./deepforest
